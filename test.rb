128267bb539fea7efca503321e7df66707c90c79
Sabrina Becker <sbecka8@gmail.com>
2020-06-05 01:14:36 -0400

    Learn ruby variables syntax

--- test.rb
+++ test.rb
@@ -1,1 +1,20 @@
 puts('Hello world!!! How are you?')
+# This is a comment
+# local variables can start with underscore or letter
+x = 10
+y = 'Hello there'
+puts(x)
+
+# global variables start with $
+$g = 'global'
+
+# nil is similar to 'undefined' in JS
+z = nil
+
+# concat does not convert data types, need to specify conversion
+a = x.to_s + y # 10Hello there
+puts(a)
+
+if (x != z)
+    puts('okay!')
+end
