e9eebf430f0e478a67afb5f71a06041199c7b8a1
Sabrina Becker <sbecka8@gmail.com>
2020-06-09 00:11:01 -0400

    Working on solution for queue with arrays

--- queue.rb
+++ queue.rb
@@ -1,9 +1,27 @@
 class QueueTwoStacks
     # Implement the enqueue and dequeue methods.
-    def enqueue(item)
+    # create 2 stacks
+    def initialize
+        @first_stack = []
+        @second_stack = []
+      end
       
+    def enqueue(item)
+        @first_stack.push(item)
     end
-  
+
     def dequeue
+        if @second_stack.empty?
+            until @first_stack.empty?
+            new_item = @first_stack.pop
+            @second_stack.push(new_item)
+            end
+        end
+        
+        if @second_stack.empty?
+            raise "Can't dequeue from empty queue!"
+        else
+            @second_stack.pop
+        end
     end
 end
